f10542423b2524e66dd25d875146dfb812d77884
Spelchure <alperenbayramoglu2@gmail.com>
2023-05-22 21:10:46 +0300

    refactor: removed comments

--- src/shared/timestamp.ts
+++ src/shared/timestamp.ts
@@ -1,13 +1,7 @@
-/**
- * Iso8601 standart date time
- *  Value object
- *  should be immutable
- */
 export class Timestamp {
   private _timestamp: string;
 
   constructor(timestamp?: string) {
-    //assert(timestamp is valid)
     if (timestamp !== undefined) this._timestamp = timestamp;
     else this._timestamp = new Date().toISOString();
   }
