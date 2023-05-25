4a0443dfc87fddb74d54c24abf5c4d7a0382873c
Spelchure <alperenbayramoglu2@gmail.com>
2023-05-25 21:59:59 +0300

    feat: implement logging with winston #7

--- src/shared/logging.ts
+++ src/shared/logging.ts
@@ -1,31 +1,52 @@
 import {injectable} from 'inversify';
+import winston, {format} from 'winston';
 
 export type LogMessage = string;
 
-export enum LogTypes {
-  INFO = 'INFO',
-  WARN = 'WARN',
-  ERROR = 'ERROR',
+export enum LogLevel {
+  INFO = 'info',
+  WARN = 'warn',
+  ERROR = 'error',
 }
+
+const alignColorsAndTime = winston.format.combine(
+  winston.format.timestamp({
+    format: 'DD.MM.YYYY HH:mm:ss',
+  }),
+  winston.format.printf(
+    info => `[${info.timestamp}] [${info.level.toUpperCase()}]: ${info.message}`
+  )
+);
 
 @injectable()
 export class Logging {
   private _prefix: string;
+  private _logger: winston.Logger;
 
   constructor(prefix = '') {
     this._prefix = prefix;
+    this._logger = this._initializeWinston();
   }
 
-  private _log(msg: LogMessage, type: LogTypes) {
-    console.log(`[${this._prefix}] [${type}]: ${msg}`);
-  }
   public logInfo(msg: LogMessage) {
-    this._log(msg, LogTypes.INFO);
+    this._log(msg, LogLevel.INFO);
   }
   public logWarn(msg: LogMessage) {
-    this._log(msg, LogTypes.WARN);
+    this._log(msg, LogLevel.WARN);
   }
   public logError(msg: LogMessage) {
-    this._log(msg, LogTypes.ERROR);
+    this._log(msg, LogLevel.ERROR);
+  }
+
+  private _log(msg: LogMessage, level: LogLevel) {
+    this._logger.log(level, `${this._prefix} ${msg}`);
+  }
+
+  private _initializeWinston() {
+    const logger = winston.createLogger({
+      format: format.combine(alignColorsAndTime, format.colorize({all: true})),
+      transports: new winston.transports.Console(),
+    });
+    return logger;
   }
 }
