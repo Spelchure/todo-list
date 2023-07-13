035ec1a337bf838bf98616ec78a2b877dca578fc
Spelchure <alperenbayramoglu2@gmail.com>
2023-07-13 20:21:39 +0300

    feat: started to improve winston logging

--- src/shared/logging.ts
+++ src/shared/logging.ts
@@ -4,23 +4,17 @@ import winston, {format} from 'winston';
 export type LogMessage = string;
 
 export enum LogLevel {
+  DEBUG = 'debug',
   INFO = 'info',
   WARN = 'warn',
   ERROR = 'error',
 }
 
-const alignColorsAndTime = winston.format.combine(
-  winston.format.timestamp({
-    format: 'DD.MM.YYYY HH:mm:ss',
-  }),
-  winston.format.printf(
-    info => `[${info.timestamp}] [${info.level.toUpperCase()}]: ${info.message}`
-  )
-);
-
 @injectable()
 export class Logging {
   private _prefix: string;
+  private static _appName = 'TodoList';
+  private static _logFilePath = './logs.json';
   private _logger: winston.Logger;
 
   constructor(prefix = '') {
@@ -37,6 +31,11 @@ export class Logging {
   public logError(msg: LogMessage) {
     this._log(msg, LogLevel.ERROR);
   }
+  public logDebug(msg: LogMessage) {
+    if (process.env.NODE_ENV !== 'production') {
+      this._log(msg, LogLevel.DEBUG);
+    }
+  }
 
   private _log(msg: LogMessage, level: LogLevel) {
     this._logger.log(level, `${this._prefix} ${msg}`);
@@ -44,9 +43,65 @@ export class Logging {
 
   private _initializeWinston() {
     const logger = winston.createLogger({
-      format: format.combine(alignColorsAndTime, format.colorize({all: true})),
-      transports: new winston.transports.Console(),
+      transports: Logging._getTransports(Logging._logFilePath),
     });
     return logger;
+  }
+
+  private static _getFormat() {
+    return this._getFormatterByEnvironment();
+  }
+
+  private static _getFormatterByEnvironment() {
+    let formats = [format.timestamp()];
+
+    if (process.env.NODE_ENV !== 'production') {
+      formats = [...formats];
+      formats = [...formats, format.json()];
+    } else {
+      formats = [...formats];
+    }
+    return winston.format.combine(...formats);
+  }
+
+  private static _getTransports(logFilePath: string) {
+    const transports: Array<any> = [
+      new winston.transports.Console({
+        format: this._getFormatForConsole(),
+      }),
+    ];
+
+    if (process.env.NODE_ENV !== 'production') {
+      transports.push(
+        new winston.transports.File({
+          filename: logFilePath,
+          format: this._getFormatForFile(),
+        })
+      );
+    }
+
+    return transports;
+  }
+
+  private static _getFormatForConsole() {
+    return format.combine(
+      format.timestamp(),
+      format.printf(
+        info =>
+          `[${info.timestamp}] [${info.level.toUpperCase()}]: ${info.message}`
+      ),
+      format.colorize({all: true}) // FIX: set specific colors.
+    );
+  }
+
+  private static _getFormatForFile() {
+    return format.combine(
+      format.timestamp(),
+      format(info => {
+        info.app = this._appName;
+        return info;
+      })(),
+      format.json()
+    );
   }
 }
